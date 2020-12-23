d843e93ec0b119d81ed955c610f056e1833ad24f
Promise Fru <promisefru1@gmail.com>
2020-12-23 13:08:15 +0100

    updated

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -35,32 +35,32 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.fail(12, '12', 'numbers are coerced into strings with == ');
-      assert.fail({
+      assert.equal(12, '12', 'numbers are coerced into strings with == ');
+      assert.equal({
         value: 1
       }, {
         value: 1
       }, '== compares object references');
-      assert.fail(6 * '2', '12', 'no more hints...');
-      assert.fail(6 + '2', '12', 'type your error message if you want');
+      assert.notEqual(6 * '2', '12', 'no more hints...');
+      assert.notEqual(6 + '2', '12', 'type your error message if you want');
     });
     // #6
     test('#strictEqual, #notStrictEqual', function () {
-      assert.fail(6, '6');
-      assert.fail(6, 3 * 2);
-      assert.fail(6 * '2', 12);
-      assert.fail([1, 'a', {}], [1, 'a', {}]);
+      assert.notStrictEqual(6, '6');
+      assert.strictEqual(6, 3 * 2);
+      assert.notStrictEqual(6 * '2', 12);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.fail({
+      assert.deepEqual({
         a: '1',
         b: 5
       }, {
         b: 5,
         a: '1'
       }, "keys order doesn't matter");
-      assert.fail({
+      assert.notDeepEqual({
         a: [5, 6]
       }, {
         a: [6, 5]
