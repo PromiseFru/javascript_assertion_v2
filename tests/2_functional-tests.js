0f3ccb944bcbd25604f334ca4fe2fdae918f6737
Promise Fru <promisefru1@gmail.com>
2020-12-24 15:54:59 +0100

    more updates

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -55,6 +55,9 @@ suite("Functional Tests", function () {
         })
         .end(function (err, res) {
           assert.equal(res.status, 200);
+          assert.equal(res.type, "application/json");
+          assert.equal(res.body.name, "Giovanni");
+          assert.equal(res.body.surname, "da Verrazzano");
           done();
         });
     });
